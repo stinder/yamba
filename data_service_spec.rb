9f4984186a8b692264aab9ec1d719ab51ec9335f
Stefanie Tinder <stinder@thoughtworks.com>
2013-03-11 22:29:47 +0000

    gitignore

---
+++ data_service_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+describe "Data Service" do
+
+  it "should find appropriate stop times" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
